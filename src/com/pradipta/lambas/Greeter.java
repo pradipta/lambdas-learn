7f0f0bdb0c6e5b894b49ff804ce0082037aed499
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-05 22:39:52 +0530

    initial

--- src/com/pradipta/lambas/Greeter.java
+++ src/com/pradipta/lambas/Greeter.java
@@ -11,5 +11,17 @@ public class Greeter {
         //greeter.greet(new HelloWorldGreeting());
         MyLambda lambda = (int a, int b) -> a+b;
         greeter.greet(lambda);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running in thread 1");
+            }
+        });
+
+        Thread thread2 = new Thread(() -> System.out.println("Running in thread 2"));
+
+        thread1.run();
+        thread2.run();
     }
 }
